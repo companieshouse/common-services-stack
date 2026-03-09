a778419642e1200324fd5a5486b84d2394541d57
bcullerton <bcullerton@companieshouse.gov.uk>
2026-03-09 15:02:55 +0000

    Group module vars logically

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -28,12 +28,14 @@ module "ecs_cluster" {
   vpc_id                      = data.aws_vpc.vpc.id
   subnet_ids                  = local.application_subnet_ids
   ec2_key_pair_name           = var.ec2_key_pair_name
+  enable_container_insights   = var.enable_container_insights
+
   asg_max_instance_count      = var.asg_max_instance_count
   asg_min_instance_count      = var.asg_min_instance_count
-  enable_container_insights   = var.enable_container_insights
   asg_desired_instance_count  = var.asg_desired_instance_count
   scaledown_schedule          = var.asg_scaledown_schedule
   scaleup_schedule            = var.asg_scaleup_schedule
   enable_asg_autoscaling      = var.enable_asg_autoscaling
+
   notify_topic_slack_endpoint = local.notify_topic_slack_endpoint
 }
