6a5c9c27ceae10ae6d6ddd366aed7c5f5af7e94d
bcullerton <bcullerton@companieshouse.gov.uk>
2026-03-09 14:56:24 +0000

    space variables

--- groups/infrastructure/variables.tf
+++ groups/infrastructure/variables.tf
@@ -3,11 +3,13 @@ variable "environment" {
   type        = string
   description = "The environment name, defined in envrionments vars."
 }
+
 variable "aws_region" {
   default     = "eu-west-2"
   type        = string
   description = "The AWS region for deployment."
 }
+
 variable "aws_profile" {
   default     = "development-eu-west-2"
   type        = string
